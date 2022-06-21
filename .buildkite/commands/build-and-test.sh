8a0f3079ec6ea89a2c9844d75ed943eb66913faa
Gio Lodi <giovanni.lodi42@gmail.com>
2022-06-21 15:02:41 +1000

    DEBUG – Add check for `BUILDKITE_ANALYTICS_TOKEN` availability

--- .buildkite/commands/build-and-test.sh
+++ .buildkite/commands/build-and-test.sh
@@ -7,5 +7,14 @@ gem install bundler
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
+echo "--- Verify BUILDKITE_ANALYTICS_TOKEN"
+if [ -z "$BUILDKITE_ANALYTICS_TOKEN" ]; then
+  # This should not run because of the `-u` in the shebang, but just in case...
+  echo "+++ Token not available!"
+  exit 1
+else
+  echo "+++ Test Analytics token found, moving on"
+fi
+
 echo "--- :hammer_and_wrench: Building and testing"
 bundle exec fastlane test
