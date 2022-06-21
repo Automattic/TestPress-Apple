aec3188394b5ac904b21a4ccbf367cf69f71249e
Gio Lodi <giovanni.lodi42@gmail.com>
2022-06-21 14:43:03 +1000

    Update Buildkite pipeline to use Fastlane for testing

--- .buildkite/commands/build-and-test.sh
+++ .buildkite/commands/build-and-test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -eu
 
+# Workaround for https://github.com/Automattic/buildkite-ci/issues/79
+echo "--- :rubygems: Fixing Ruby Setup"
+gem install bundler
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
 echo "--- :hammer_and_wrench: Building and testing"
-xcodebuild clean test \
-  -project 'TestPress.xcodeproj' \
-  -scheme 'TestPress' \
-  -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.5'
+bundle exec fastlane test
